c986fbd4573e9f98b559c823fa8e42f77e45c0d2
btopro <bto108@psu.edu>
2019-09-17 14:32:47 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,4 +3,3 @@ import './lit-example-card.js';
 import './polymer-example-card.js';
 import './wired-data.js';
 import '@polymer/paper-button/paper-button.js';
-import '@lrnwebcomponents/video-player/video-player.js';
